6f4a52f9211db2ee2c1d2ff94ab6133620b57740
ZhuiyiTechnology <51402681+ZhuiyiTechnology@users.noreply.github.com>
2020-01-27 23:10:52 +0800

    Create task_sentiment.py

---
+++ examples/task_sentiment.py
@@ -0,0 +1,150 @@
+#! -*- coding:utf-8 -*-
+# 情感分析例子
+
+import json
+from io import open
+import numpy as np
+from bert4keras.backend import keras, set_gelu
+from bert4keras.tokenizer import Tokenizer
+from bert4keras.bert import build_bert_model
+from bert4keras.optimizers import Adam, extend_with_piecewise_linear_lr
+from bert4keras.snippets import sequence_padding, DataGenerator
+from keras.layers import *
+
+set_gelu('tanh')  # 切换gelu版本
+
+
+num_classes = 2
+maxlen = 128
+batch_size = 32
+
+# RoBERTa small
+config_path = '/root/kg/bert/chinese_roberta_L-6_H-384_A-12/bert_config.json'
+checkpoint_path = '/root/kg/bert/chinese_roberta_L-6_H-384_A-12/bert_model.ckpt'
+dict_path = '/root/kg/bert/chinese_roberta_L-6_H-384_A-12/vocab.txt'
+model_type = 'bert'
+
+"""
+# albert small
+config_path = '/root/kg/bert/albert_small_zh_google/albert_config.json'
+checkpoint_path = '/root/kg/bert/albert_small_zh_google/albert_model.ckpt'
+dict_path = '/root/kg/bert/albert_small_zh_google/vocab.txt'
+model_type = 'albert'
+
+# RoBERTa tiny
+config_path = '/root/kg/bert/chinese_roberta_L-4_H-312_A-12/bert_config.json'
+checkpoint_path = '/root/kg/bert/chinese_roberta_L-4_H-312_A-12/bert_model.ckpt'
+dict_path = '/root/kg/bert/chinese_roberta_L-4_H-312_A-12/vocab.txt'
+model_type = 'bert'
+
+# albert tiny
+config_path = '/root/kg/bert/albert_tiny_zh_google/albert_config.json'
+checkpoint_path = '/root/kg/bert/albert_tiny_zh_google/albert_model.ckpt'
+dict_path = '/root/kg/bert/albert_tiny_zh_google/vocab.txt'
+model_type = 'albert'
+"""
+
+
+def load_data(filename):
+    D = []
+    with open(filename, encoding='utf-8') as f:
+        for l in f:
+            text, label = l.strip().split('\t')
+            D.append((text, int(label)))
+    return D
+
+
+# 加载数据集
+train_data = load_data('datasets/sentiment/sentiment.train.data')
+valid_data = load_data('datasets/sentiment/sentiment.valid.data')
+test_data = load_data('datasets/sentiment/sentiment.test.data')
+
+# 建立分词器
+tokenizer = Tokenizer(dict_path, do_lower_case=True)
+
+
+class data_generator(DataGenerator):
+    """数据生成器
+    """
+    def __iter__(self, random=False):
+        idxs = list(range(len(self.data)))
+        if random:
+            np.random.shuffle(idxs)
+        batch_token_ids, batch_segment_ids, batch_labels = [], [], []
+        for i in idxs:
+            text, label = self.data[i]
+            token_ids, segment_ids = tokenizer.encode(text, max_length=maxlen)
+            batch_token_ids.append(token_ids)
+            batch_segment_ids.append(segment_ids)
+            batch_labels.append([label])
+            if len(batch_token_ids) == self.batch_size or i == idxs[-1]:
+                batch_token_ids = sequence_padding(batch_token_ids)
+                batch_segment_ids = sequence_padding(batch_segment_ids)
+                batch_labels = sequence_padding(batch_labels)
+                yield [batch_token_ids, batch_segment_ids], batch_labels
+                batch_token_ids, batch_segment_ids, batch_labels = [], [], []
+
+
+# 加载预训练模型
+bert = build_bert_model(
+    config_path=config_path,
+    checkpoint_path=checkpoint_path,
+    model=model_type,
+    return_keras_model=False,
+)
+
+output = Lambda(lambda x: x[:, 0], name='CLS-token')(bert.model.output)
+output = Dense(units=num_classes,
+               activation='softmax',
+               kernel_initializer=bert.initializer)(output)
+
+model = keras.models.Model(bert.model.input, output)
+model.summary()
+AdamLR = extend_with_piecewise_linear_lr(Adam)
+
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    # optimizer=Adam(1e-5),  # 用足够小的学习率
+    optimizer=AdamLR(learning_rate=1e-4,
+                     lr_schedule={1000: 1, 2000: 0.1}),
+    metrics=['accuracy'],
+)
+
+# 转换数据集
+train_generator = data_generator(train_data, batch_size)
+valid_generator = data_generator(valid_data, batch_size)
+test_generator = data_generator(test_data, batch_size)
+
+
+def evaluate(data):
+    total, right = 0., 0.
+    for x_true, y_true in data:
+        y_pred = model.predict(x_true).argmax(axis=1)
+        y_true = y_true[:, 0]
+        total += len(y_true)
+        right += (y_true == y_pred).sum()
+    return right / total
+
+
+class Evaluator(keras.callbacks.Callback):
+    def __init__(self):
+        self.best_val_acc = 0.
+
+    def on_epoch_end(self, epoch, logs=None):
+        val_acc = evaluate(valid_generator)
+        if val_acc > self.best_val_acc:
+            self.best_val_acc = val_acc
+            model.save_weights('best_model.weights')
+        test_acc = evaluate(test_generator)
+        print(u'val_acc: %.5f, best_val_acc: %.5f, test_acc: %.5f\n' %
+              (val_acc, self.best_val_acc, test_acc))
+
+
+evaluator = Evaluator()
+model.fit_generator(train_generator.forfit(),
+                    steps_per_epoch=len(train_generator),
+                    epochs=10,
+                    callbacks=[evaluator])
+
+model.load_weights('best_model.weights')
+print(u'final test acc: %05f\n' % (evaluate(test_generator)))
