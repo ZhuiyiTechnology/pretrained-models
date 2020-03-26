f30cc0792741fa4bb046f69727d84f531526da8b
ZhuiyiTechnology <51402681+ZhuiyiTechnology@users.noreply.github.com>
2020-03-26 17:31:43 +0800

    Update simbert_base.py

--- examples/simbert_base.py
+++ examples/simbert_base.py
@@ -4,11 +4,11 @@
 import numpy as np
 from collections import Counter
 from bert4keras.backend import keras, K
-from bert4keras.bert import build_bert_model
-from bert4keras.tokenizer import Tokenizer
-from bert4keras.snippets import sequence_padding
+from bert4keras.models import build_transformer_model
+from bert4keras.tokenizers import Tokenizer
+from bert4keras.snippets import sequence_padding, AutoRegressiveDecoder
+from bert4keras.snippets import uniout
 from keras.layers import *
-
 
 maxlen = 32
 
@@ -21,11 +21,11 @@ dict_path = '/root/kg/bert/chinese_simbert_L-12_H-768_A-12/vocab.txt'
 tokenizer = Tokenizer(dict_path, do_lower_case=True)  # 建立分词器
 
 # 建立加载模型
-bert = build_bert_model(
+bert = build_transformer_model(
     config_path,
     checkpoint_path,
     with_pool='linear',
-    application='seq2seq',
+    application='unilm',
     return_keras_model=False,
 )
 
@@ -33,39 +33,34 @@ encoder = keras.models.Model(bert.model.inputs, bert.model.outputs[0])
 seq2seq = keras.models.Model(bert.model.inputs, bert.model.outputs[1])
 
 
-def random_generate(s, n=5):
-    """通过随机采样来seq2seq生成
+class SynonymsGenerator(AutoRegressiveDecoder):
+    """seq2seq解码器
     """
-    token_ids, segment_ids = tokenizer.encode(s)
-    token_count = Counter(token_ids)
-    target_ids = [[] for _ in range(n)]
-    R = []
-    for i in range(maxlen):
-        _target_ids = [token_ids + t for t in target_ids]
-        _segment_ids = [segment_ids + [1] * len(t) for t in target_ids]
-        # 下面直接忽略[PAD], [UNK], [CLS]
-        _probas = seq2seq.predict([_target_ids, _segment_ids])[:, -1, 3:]
-        for i, p in enumerate(_probas):
-            target_count = Counter(target_ids[i])
-            for j, k in target_count.items():
-                if token_count.get(j, 2) - k == 0:
-                    p[j - 3] *= 0
-            p /= sum(p)
-            target_ids[i].append(np.random.choice(len(p), p=p) + 3)
-        for t in target_ids:
-            if t[-1] == 3:
-                R.append(tokenizer.decode(t))
-        target_ids = [t for t in target_ids if t[-1] != 3]
-        if len(target_ids) == 0:
-            break
-    return R
+    @AutoRegressiveDecoder.set_rtype('probas')
+    def predict(self, inputs, output_ids, step):
+        token_ids, segment_ids = inputs
+        token_ids = np.concatenate([token_ids, output_ids], 1)
+        segment_ids = np.concatenate(
+            [segment_ids, np.ones_like(output_ids)], 1)
+        return seq2seq.predict([token_ids, segment_ids])[:, -1]
+
+    def generate(self, text, n=1, topk=5):
+        token_ids, segment_ids = tokenizer.encode(text, max_length=maxlen)
+        output_ids = self.random_sample([token_ids, segment_ids], n, topk)  # 基于随机采样
+        return [tokenizer.decode(ids) for ids in output_ids]
+
+
+synonyms_generator = SynonymsGenerator(start_id=None,
+                                       end_id=tokenizer._token_end_id,
+                                       maxlen=maxlen)
 
 
 def gen_synonyms(text, n=100, k=20):
     """"含义： 产生sent的n个相似句，然后返回最相似的k个。
     做法：用seq2seq生成，并用encoder算相似度并排序。
     """
-    r = [i for i in set(random_generate(text, n)) if i != text]
+    r = synonyms_generator.generate(text, n)
+    r = [i for i in set(r) if i != text]
     r = [text] + r
     X, S = [], []
     for t in r:
